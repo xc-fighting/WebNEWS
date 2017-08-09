d0becec6d3ab17d9898c1de1279e485cbadc3e96
Chen Xu <cxu264@usc.edu>
2017-08-09 14:06:05 +0800

    make some modification to home activity

--- client/app/src/main/java/com/xuchen/news/HomeActivity.java
+++ client/app/src/main/java/com/xuchen/news/HomeActivity.java
@@ -1,12 +1,13 @@
 package com.xuchen.news;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HomeActivity extends ActionBarActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,25 +15,5 @@ public class HomeActivity extends ActionBarActivity {
         setContentView(R.layout.activity_home);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
